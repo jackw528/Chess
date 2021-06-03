757470e0e626dbb0a5976b9efce55a89045d6a8e
jackw528 <43789827+jackw528@users.noreply.github.com>
2021-06-02 22:25:56 -0400

    Update Pawn.java

--- src/Pawn.java
+++ src/Pawn.java
@@ -13,7 +13,7 @@ public class Pawn extends Piece implements ChessPiece{
 
         if(currentRow == row - 1 && currentCol == col){
             if(row == 8){
-                //some sort of promotion prompt
+  
             }
             return true;
 
@@ -25,9 +25,8 @@ public class Pawn extends Piece implements ChessPiece{
     public boolean canKill(int row, int col) {
         int currentRow = this.getRow();
         int currentCol = this.getColumn();
-        if(currentRow == row + 1 && Math.abs(currentCol - col) == 1 ){ //add an en passant condition as well
-            //condition here to check if the piece occupying that space is of the other color
-            return true; //filler for now just so it doesn't error out
+        if(currentRow == row + 1 && Math.abs(currentCol - col) == 1 ){
+            return true; 
         }else{
             return false;
         }    }
