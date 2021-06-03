3babe4dcfade4b6590d62f4e0eb9f016198b6c84
jackw528 <43789827+jackw528@users.noreply.github.com>
2021-06-02 22:26:42 -0400

    Update Queen.java

--- src/Queen.java
+++ src/Queen.java
@@ -9,15 +9,9 @@ public class Queen extends Piece implements ChessPiece{
         if(row > 9 || col > 9) {
             return false;
         }
-        //you would  check to see whether there is a piece currently occupying this spot as well
-        //since I am not creating a class for the board, I am not implementing this check
         int currentRow = this.getRow();
         int currentCol = this.getColumn();
-        //conditions for queen are a bit more complicated
-        //first condition is a lateral movement
-        //second condition is vertical movement
-        //third condition is diagonal movement
-        //Also need to make sure that there are no pieces in the path of this movement
+
         if(currentRow == row || currentCol == col || (Math.abs(currentRow-row) == Math.abs(currentCol - col))){
             return true;
         }else{
@@ -28,10 +22,7 @@ public class Queen extends Piece implements ChessPiece{
     @Override
     public boolean canKill(int row, int col) {
         if(canMove(row, col)){
-            //condition here to check if the piece occupying that space is of the other color
-            //if it is a different color and the king is not put in check as a result, return true
-            //else return false
-            return true; //filler for now just so it doesn't error out
+            return true;
         }else{
             return false;
         }
